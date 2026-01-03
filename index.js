5cb02896d2b7651332d2699beb7e9fa385c5e85b
Sabbir Hossain Mondol <sabbirhossain25112000@gmail.com>
2026-01-03 16:50:52 +0600

    find maximum number by js

---
+++ index.js
@@ -0,0 +1,15 @@
+function highestScore() {
+    let score = [];
+    for(let i=0;i<5;i++){
+        score[i] = parseInt(prompt("Enter a number: "));
+    }
+
+        let max = score[0];
+  for (let i = 1; i < 5; i++) {
+    if (max < score[i]) {
+      max = score[i];
+    }
+  }
+  return max;
+}
+console.log(highestScore()); // Output: 95
